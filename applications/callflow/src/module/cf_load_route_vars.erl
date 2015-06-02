465b7c3e62948b391f2a33728919480aa4ee64c4
Daniel Finke <danielfinke2011@gmail.com>
2015-06-02 14:11:45 -0700

    Added all dynamic modules and updated load point for CCVs (Closed #30)

--- applications/callflow/src/module/cf_load_route_vars.erl
+++ applications/callflow/src/module/cf_load_route_vars.erl
@@ -11,42 +11,20 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec handle(wh_json:object(), whapps_call:call()) -> 'ok'.
-handle(_Data, Call) ->
-    AccountId = whapps_call:account_id(Call),
-    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
-    FlowId = whapps_call:kvs_fetch('cf_flow_id', Call),
-
-    Call2 = maybe_load_route_vars(AccountDb, FlowId, Call),
+handle(Data, Call) ->
+    Call2 = load_route_vars(Data, Call),
     cf_exe:set_call(Call2),
     cf_exe:continue(Call2).
 
--spec maybe_load_route_vars(api_binary(), api_binary(), whapps_call:call()) -> whapps_call:call().
-maybe_load_route_vars(AccountDb, FlowId, Call) ->
-    case couch_mgr:open_doc(AccountDb, <<"route_vars_mapping">>) of
-        {'ok', MappingDoc} ->
-            %% Can safely assume callflow doc exists since we got here
-            {'ok', FlowDoc} = couch_mgr:open_doc(AccountDb, FlowId),
-            load_route_vars(MappingDoc, FlowDoc, Call);
-        {'error', E} ->
-            lager:debug("could not open route vars mapping doc (~p)", [E]),
-            Call
-    end.
+-spec load_route_vars(wh_json:object(), whapps_call:call()) -> whapps_call:call().
+load_route_vars(Data, Call) ->
+    load_route_vars_fold(wh_json:get_keys(Data), Data, Call).
 
--spec load_route_vars(wh_json:object(), wh_json:object(), whapps_call:call()) -> whapps_call:call().
-load_route_vars(MappingDoc, FlowDoc, Call) ->
-    load_route_vars_fold(MappingDoc, wh_json:get_value(<<"numbers">>, FlowDoc, []), Call).
-
--spec load_route_vars_fold(wh_json:object(), list(), whapps_call:call()) -> whapps_call:call().
-load_route_vars_fold(_, [], Call) ->
+-spec load_route_vars_fold(list(), wh_json:object(), whapps_call:call()) -> whapps_call:call().
+load_route_vars_fold([], _, Call) ->
     Call;
-load_route_vars_fold(MappingDoc, [Number|Numbers], Call) ->
-    NumberCCVs = wh_json:get_value(Number, MappingDoc, wh_json:new()),
-    Call3 = lists:foldl(
-              fun(Key, Call2) ->
-                                Value = wh_json:get_value(Key, NumberCCVs),
-                                lager:debug("setting route var ~p to ~p", [Key, Value]),
-                                whapps_call:set_custom_channel_var(Key, Value, Call2)
-              end
-              ,Call
-              ,wh_json:get_keys(NumberCCVs)),
-    load_route_vars_fold(MappingDoc, Numbers, Call3).
+load_route_vars_fold([Key|Keys], Data, Call) ->
+    Value = wh_json:get_value(Key, Data),
+    lager:debug("setting route var ~p to ~p", [Key, Value]),
+    Call2 = whapps_call:set_custom_channel_var(Key, Value, Call),
+    load_route_vars_fold(Keys, Data, Call2).
