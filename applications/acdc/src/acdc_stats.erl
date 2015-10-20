ef2bbe06900426779dcabf4cbbaff9e4a9f14bad
Daniel Finke <danielfinke2011@gmail.com>
2015-10-20 15:11:14 -0700

    PISTON-118: acdc_stats_util:cleanup_old_data removes older than 1h calls

--- applications/acdc/src/acdc_stats.erl
+++ applications/acdc/src/acdc_stats.erl
@@ -32,6 +32,7 @@
          ,agent_outbound/3
 
          ,agent_statuses/0
+         ,manual_cleanup/1
         ]).
 
 %% ETS config
@@ -242,6 +243,44 @@ agent_outbound(AcctId, AgentId, CallId) ->
 -spec agent_statuses() -> ne_binaries().
 agent_statuses() ->
     ?STATUS_STATUSES.
+
+-spec manual_cleanup(pos_integer()) -> 'ok'.
+manual_cleanup(Window) ->
+    Srv = call_table_id(),
+    
+    Past = wh_util:current_tstamp() - Window,
+    PastConstraint = {'=<', '$1', Past},
+
+    TypeConstraints = [{'=/=', '$2', {'const', <<"waiting">>}}
+                       ,{'=/=', '$2', {'const', <<"handled">>}}
+                      ],
+
+    CallMatch = [{#call_stat{entered_timestamp='$1', status='$2', _='_'}
+                  ,[PastConstraint | TypeConstraints]
+                  ,['$_']
+                 }],
+    gen_listener:cast(Srv, {'remove_call', CallMatch}),
+
+    StatusMatch = [{#status_stat{timestamp='$1', _='_'}
+                    ,[{'=<', '$1', Past}]
+                    ,['$_']
+                   }],
+    gen_listener:cast(Srv, {'remove_status', StatusMatch}),
+
+    case ets:select(?MODULE:call_table_id()
+                    ,[{#call_stat{entered_timestamp='$1', status= <<"waiting">>, _='_'}
+                       ,[PastConstraint]
+                       ,['$_']
+                      }
+                      ,{#call_stat{entered_timestamp='$1', status= <<"handled">>, _='_'}
+                        ,[PastConstraint]
+                        ,['$_']
+                       }
+                     ])
+    of
+        [] -> 'ok';
+        Unfinished -> cleanup_unfinished(Unfinished)
+    end.
 
 %% ETS config
 call_table_id() -> 'acdc_stats_call'.
