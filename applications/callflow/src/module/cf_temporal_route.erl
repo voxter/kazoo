4a062c686b4f7c48d91d8a8c9c6acc6d548f7dd7
Daniel Finke <danielfinke2011@gmail.com>
2016-05-06 14:37:58 -0700

    PISTON-260: time conditions can have date ranges

--- applications/callflow/src/module/cf_temporal_route.erl
+++ applications/callflow/src/module/cf_temporal_route.erl
@@ -203,15 +203,30 @@ occurrence_rate(#rule{cycle = <<"weekly">>
     (Stop - Start) * (365 / 7) * length(Weekdays) / I0;
 occurrence_rate(#rule{cycle = <<"monthly">>
                       ,interval=I0
+                      ,wdays=Weekdays
                       ,wtime_start=Start
                       ,wtime_stop=Stop
-                     }) ->
+                     }) when length(Weekdays) > 0 ->
     (Stop - Start) * 12 / I0;
-occurrence_rate(#rule{cycle = <<"yearly">>
+occurrence_rate(#rule{cycle = <<"monthly">>
+                      ,interval=I0
+                      ,days=Days
                       ,wtime_start=Start
                       ,wtime_stop=Stop
                      }) ->
-    (Stop - Start).
+    (Stop - Start) * 12 / I0 * length(Days);
+occurrence_rate(#rule{cycle = <<"yearly">>
+                      ,wdays=Weekdays
+                      ,wtime_start=Start
+                      ,wtime_stop=Stop
+                     }) when length(Weekdays) > 0 ->
+    (Stop - Start);
+occurrence_rate(#rule{cycle = <<"yearly">>
+                      ,days=Days
+                      ,wtime_start=Start
+                      ,wtime_stop=Stop
+                     }) ->
+    (Stop - Start) * length(Days).
 
 %%--------------------------------------------------------------------
 %% @private
