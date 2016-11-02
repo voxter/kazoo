55017fa220877d3fe035be3f866cf2a51281d9c5
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-02 10:47:31 -0700

    KAZOO-5127: bring back PUT /v1,2/.../phone_numbers/Num/port (#2731)
    * KAZOO-5127: give resellers the right to create port_in numbers
    
    * KAZOO-5127: add PUT /port to v1 and v2
    
    * KAZOO-5127: make apis
    
    * KAZOO-5127: update docs
    
    * KAZOO-5127: document new konfig flag

--- applications/crossbar/src/modules_v1/cb_phone_numbers_v1.erl
+++ applications/crossbar/src/modules_v1/cb_phone_numbers_v1.erl
@@ -32,6 +32,7 @@
 
 -define(ACTIVATE, <<"activate">>).
 -define(RESERVE, <<"reserve">>).
+-define(PORT, <<"port">>).
 
 -define(CLASSIFIERS, <<"classifiers">>).
 -define(IDENTIFY, <<"identify">>).
@@ -97,6 +98,8 @@ allowed_methods(_, ?ACTIVATE) ->
     [?HTTP_PUT];
 allowed_methods(_, ?RESERVE) ->
     [?HTTP_PUT];
+allowed_methods(_, ?PORT) ->
+    [?HTTP_PUT];
 allowed_methods(_, ?IDENTIFY) ->
     [?HTTP_GET];
 allowed_methods(_, ?CHANGE_CARRIER) ->
@@ -121,6 +124,7 @@ resource_exists(_) -> 'true'.
 
 resource_exists(_, ?ACTIVATE) -> 'true';
 resource_exists(_, ?RESERVE) -> 'true';
+resource_exists(_, ?PORT) -> 'true';
 resource_exists(_, ?IDENTIFY) -> 'true';
 resource_exists(_, ?CHANGE_CARRIER) -> 'true';
 resource_exists(_, _) -> 'false'.
@@ -229,6 +233,8 @@ validate_3(Context, ?HTTP_PUT, _Number, ?ACTIVATE) ->
     end;
 validate_3(Context, ?HTTP_PUT, _Number, ?RESERVE) ->
     validate_request(Context);
+validate_3(Context, ?HTTP_PUT, _Number, ?PORT) ->
+    validate_request(Context);
 validate_3(Context, ?HTTP_GET, Number, ?IDENTIFY) ->
     identify(Context, Number);
 validate_3(Context, ?HTTP_PUT, _Number, ?CHANGE_CARRIER) ->
@@ -280,6 +286,14 @@ put(Context, Number, ?RESERVE) ->
               ,{'public_fields', cb_context:doc(Context)}
               ],
     Result = knm_number:reserve(Number, Options),
+    set_response(Result, Number, Context);
+put(Context, Number, ?PORT) ->
+    Options = [{'assign_to', cb_context:account_id(Context)}
+              ,{'auth_by', cb_context:auth_account_id(Context)}
+              ,{'public_fields', cb_context:doc(Context)}
+              ,{'state', ?NUMBER_STATE_PORT_IN}
+              ],
+    Result = knm_number:create(Number, Options),
     set_response(Result, Number, Context).
 
 -spec delete(cb_context:context(), path_token()) -> cb_context:context().
