e7a33fc1e09749daccba6eb7dcdb421e932a70d3
Daniel Finke <danielfinke2011@gmail.com>
2016-11-09 05:04:14 +0000

    PISTON-351: validate did ownership before saving callflows/trunks

--- applications/crossbar/src/modules/cb_callflows.erl
+++ applications/crossbar/src/modules/cb_callflows.erl
@@ -361,11 +361,9 @@ add_pattern_conflict(Context, JObj) ->
 -spec validate_callflow_schema(api_binary(), cb_context:context()) -> cb_context:context().
 validate_callflow_schema(CallflowId, Context) ->
     OnSuccess = fun(C) ->
-                        validate_uniqueness(CallflowId
-                                           ,validate_callflow_elements(
-                                              on_successful_validation(CallflowId, C)
-                                             )
-                                           )
+                        C1 = validate_callflow_elements(on_successful_validation(CallflowId, C)),
+                        C2 = validate_uniqueness(CallflowId, C1),
+                        validate_number_ownership(C2)
                 end,
     cb_context:validate_request_data(<<"callflows">>, Context, OnSuccess).
 
@@ -382,6 +380,48 @@ validate_callflow_elements(Context) ->
     case kzd_callflow:validate_flow(cb_context:doc(Context)) of
         {'ok', CallflowJObj} -> cb_context:set_doc(Context, CallflowJObj);
         {'error', Errors} -> cb_context:failed(Context, Errors)
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec validate_number_ownership(cb_context:context()) -> cb_context:context().
+-spec validate_number_ownership(ne_binaries(), cb_context:context()) ->
+                                       cb_context:context().
+-spec validate_number_ownership(knm_number:knm_number()
+                               ,ne_binaries()
+                               ,cb_context:context()) -> cb_context:context().
+validate_number_ownership(Context) ->
+    NewNums = kz_json:get_value(<<"numbers">>, cb_context:doc(Context), []),
+
+    Assigned =  [Num
+                 || Num <- NewNums,
+                    Num =/= <<"undefined">>
+                ],
+
+    validate_number_ownership(Assigned, Context).
+
+validate_number_ownership([], Context) -> Context;
+validate_number_ownership([Number|Numbers], Context) ->
+    case knm_number:get(Number) of
+        {'ok', NumberObj} ->
+            validate_number_ownership(NumberObj, Numbers, Context);
+        _ -> validate_number_ownership(Numbers, Context)
+    end.
+
+validate_number_ownership(NumberObj, Numbers, Context) ->
+    AuthBy = cb_context:auth_account_id(Context),
+    PhoneNumber = knm_number:phone_number(NumberObj),
+    AssignedTo = knm_phone_number:assigned_to(PhoneNumber),
+    case kz_util:is_in_account_hierarchy(AuthBy, AssignedTo, 'true') of
+        'true' -> validate_number_ownership(Numbers, Context);
+        'false' ->
+            Number = knm_phone_number:number(PhoneNumber),
+            Message = <<"unauthorized to add/modify ", Number/binary>>,
+            cb_context:add_system_error(403, 'forbidden', Message, Context)
     end.
 
 %%--------------------------------------------------------------------
