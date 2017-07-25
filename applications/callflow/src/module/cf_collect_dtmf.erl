5390a7f46b1745b369ab60c1a9ae973830f1aced
Daniel Finke <danielfinke2011@gmail.com>
2017-07-25 14:27:08 +0000

    PISTON-460: remove 'global' option from binary:split

--- applications/callflow/src/module/cf_collect_dtmf.erl
+++ applications/callflow/src/module/cf_collect_dtmf.erl
@@ -81,7 +81,7 @@ collect_more_digits(Data, Call, AlreadyCollected, MaxDigits) ->
 
 -spec truncate_after_terminator(ne_binary(), ne_binaries()) -> ne_binary().
 truncate_after_terminator(AlreadyCollected, Terminators) ->
-    hd(binary:split(AlreadyCollected, Terminators, ['global'])).
+    hd(binary:split(AlreadyCollected, Terminators)).
 
 -spec collection_name(kz_json:object()) -> ne_binary().
 collection_name(Data) ->
