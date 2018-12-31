8ab49712766e5f59701d4b097fbb06ce29937377
Ben Bradford <ben@voxter.ca>
2018-12-31 23:38:08 +0000

    PISTON-650:fixed code intent issues

--- applications/tasks/src/modules/kt_vmboxes.erl
+++ applications/tasks/src/modules/kt_vmboxes.erl
@@ -182,11 +182,11 @@ import(#{account_id := Account
       ) ->
     AccountId = select_account_id(AccountId0, Account),
     Resp = case is_authorized_account(AuthAccountId, AccountId) of
-        'true' ->
-            validate_and_save_vmbox(AccountId, Args);
-        'false' ->
-            {'error', <<"Access denied, Auth Account does not have access to account">>}
-    end,
+               'true' ->
+                   validate_and_save_vmbox(AccountId, Args);
+               'false' ->
+                   {'error', <<"Access denied, Auth Account does not have access to account">>}
+           end,
     Row = handle_result(Args, Resp, 'import'),
     {Row, sets:add_element(AccountId, AccountIds)}.
 
@@ -203,11 +203,11 @@ delete(#{account_id := Account
       ) ->
     AccountId = select_account_id(AccountId0, Account),
     Resp = case is_authorized_account(AuthAccountId, AccountId) of
-        'true' ->
-            delete_vmbox(AccountId, Args);
-        'false' ->
-            {'error', <<"Access denied, Auth Account does not have access to account">>}
-    end,
+               'true' ->
+                   delete_vmbox(AccountId, Args);
+               'false' ->
+                   {'error', <<"Access denied, Auth Account does not have access to account">>}
+           end,
     Row = handle_result(Args, Resp, 'delete'),
     {Row, sets:add_element(AccountId, AccountIds)}.
 
@@ -345,7 +345,7 @@ validate_and_save_vmbox(AccountId, Args) ->
             Error;
         'ok' ->
             prepare_and_save_vmbox(AccountId, Args)
-     end.
+    end.
 
 %%------------------------------------------------------------------------------
 %% @doc Build VMBox doc and save vmbox to kazoo storage
