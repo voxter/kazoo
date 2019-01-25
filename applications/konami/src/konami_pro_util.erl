8e06c9d464f2cbd6aa5483258bd978f3c5c438b3
Daniel Finke <danielfinke2011@gmail.com>
2019-01-25 04:14:24 +0000

    PISTON-667: temporary solution to suppress konami_pro_util xref errors

--- applications/konami/src/konami_pro_util.erl
+++ applications/konami/src/konami_pro_util.erl
@@ -23,17 +23,27 @@
 -spec listen_on_other_leg(any(), any()) -> any().
 listen_on_other_leg(Arg1, Arg2) ->
     lager:debug("args: ~p", [[Arg1, Arg2]]),
-    konami_pro_hack:listen_on_other_leg(Arg1, Arg2).
+    call_hack_module('listen_on_other_leg', [Arg1, Arg2]).
 
 -spec send_hangup_req(any()) -> any().
 send_hangup_req(Arg1) ->
     lager:debug("args: ~p", [[Arg1]]),
-    konami_pro_hack:send_hangup_req(Arg1).
+    call_hack_module('send_hangup_req', [Arg1]).
 
 -spec send_break_req(any()) -> any().
 send_break_req(Arg1) ->
     lager:debug("args: ~p", [[Arg1]]),
-    konami_pro_hack:send_break_req(Arg1).
+    call_hack_module('send_break_req', [Arg1]).
+
+%%------------------------------------------------------------------------------
+%% @doc TODO: do something better to suppress xref of 'konami_pro_hack'
+%% detecting undefined_functions
+%% @end
+%%------------------------------------------------------------------------------
+-spec call_hack_module(fun(([any()]) -> any()), [any()]) -> any().
+call_hack_module(Fun, Args) ->
+    Module = 'konami_pro_hack',
+    erlang:apply(Module, Fun, Args).
 
 -spec check_license(any()) -> boolean().
 check_license(Module) ->
