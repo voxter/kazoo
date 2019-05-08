13a08971797b6dac9415c642e47a4d1a11bd2b66
Dustin Brett <dustin@voxter.ca>
2019-05-08 12:58:04 -0700

    PISTON-745: Convert /phone_numbers reqs to 'List' responses for CSV's

--- applications/crossbar/src/modules/cb_modules_util.erl
+++ applications/crossbar/src/modules/cb_modules_util.erl
@@ -35,6 +35,8 @@
         ,normalize_alphanum_name/1
 
         ,used_mac_address_message/2
+
+        ,maybe_convert_numbers_to_list/1
         ]).
 
 -include("crossbar.hrl").
@@ -485,3 +487,26 @@ used_mac_address_message(Prefix, JObj, Context) ->
         'false' ->
             Prefix
     end.
+
+-spec maybe_convert_numbers_to_list(cb_context:context()) -> cb_context:context().
+maybe_convert_numbers_to_list(Context) ->
+    case maybe_requesting_csv(Context) of
+        'true' ->
+            Numbers = kz_json:get_json_value(<<"numbers">>, cb_context:resp_data(Context)),
+            NewRespData = kz_json:foldl(fun convert_numbers_to_list/3, [], Numbers),
+            cb_context:set_resp_data(Context, NewRespData);
+        'false' -> Context
+    end.
+
+-spec maybe_requesting_csv(cb_context:context()) -> boolean().
+maybe_requesting_csv(Context) ->
+    case cb_context:req_header(Context, <<"accept">>) of
+        <<"text/csv">> -> 'true';
+        _ -> <<"csv">> =:= cb_context:req_param(Context, <<"accept">>)
+    end.
+
+-spec convert_numbers_to_list(kz_term:ne_binary(), kz_json:object(), kz_json:object()) -> kz_json:object().
+convert_numbers_to_list(Key, Value, JObj) ->
+    [kz_json:from_list([{<<"number">>, Key} | kz_json:recursive_to_proplist(Value)])
+     | JObj
+    ].
