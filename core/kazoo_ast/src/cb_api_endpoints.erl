abb76299754a4438a08b79f75dc4121924b55566
Dustin <dustin@voxter.ca>
2019-07-04 10:00:45 -0700

    PISTON-822: Crossbar module for Tray.io

--- core/kazoo_ast/src/cb_api_endpoints.erl
+++ core/kazoo_ast/src/cb_api_endpoints.erl
@@ -897,6 +897,7 @@ def_path_param(<<"{RATE_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{RESOURCE_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{RESOURCE_TEMPLATE_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{SMS_ID}">>=P) -> generic_id_path_param(P);
+def_path_param(<<"{SOLUTION_INSTANCE_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{STORAGE_PLAN_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{TEMPLATE_ID}">>=P) -> generic_id_path_param(P);
 def_path_param(<<"{TEMPORAL_RULE_ID}">>=P) -> generic_id_path_param(P);
