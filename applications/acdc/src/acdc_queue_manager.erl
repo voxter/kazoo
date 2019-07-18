382b86be7511a5f5377586746fd3f0c3a93c1ec0
Daniel Finke <danielfinke2011@gmail.com>
2019-07-18 17:50:49 +0000

    PISTON-867: remove acdc_queue_manager monitor_call
    No longer need to bind to call.CHANNEL_DESTROY.<call_id> for queue
    manager. Removal of calls is handled by other events. These bindings
    were being leaked and consuming extra AMQP memory over time
    
    Approved-by: Spencer Hutchinson <spencer@voxter.ca>
    Approved-by: Dustin Brett <dustin@voxter.ca>
    Approved-by: Jon Braz <jon@voxter.ca>
    Approved-by: Ben Bradford <ben@voxter.ca>
    Approved-by: Dayton Turner <dayton@voxter.ca>
    Approved-by: Aisling Connolly <aisling@voxter.ca>

--- applications/acdc/src/acdc_queue_manager.erl
+++ applications/acdc/src/acdc_queue_manager.erl
@@ -508,13 +508,6 @@ handle_cast({'member_call_cancel', K, JObj}, #state{ignored_member_calls=Dict
             {'noreply', State#state{ignored_member_calls=dict:store(K, 'true', Dict)}}
     end;
 
-handle_cast({'monitor_call', Call}, State) ->
-    CallId = kapps_call:call_id(Call),
-    gen_listener:add_binding(self(), 'call', [{'callid', CallId}
-                                             ,{'restrict_to', [<<"CHANNEL_DESTROY">>]}
-                                             ]),
-    lager:debug("bound for call events for ~s", [CallId]),
-    {'noreply', State};
 handle_cast({'start_workers'}, #state{account_id=AccountId
                                      ,queue_id=QueueId
                                      ,supervisor=QueueSup
@@ -689,8 +682,6 @@ handle_cast({'add_queue_member', JObj}, #state{account_id=AccountId
     %% Add call to shared queue
     kapi_acdc_queue:publish_shared_member_call(AccountId, QueueId, JObj1),
     lager:debug("put call into shared messaging queue"),
-
-    gen_listener:cast(self(), {'monitor_call', Call1}),
 
     acdc_util:presence_update(AccountId, QueueId, ?PRESENCE_RED_FLASH),
 
